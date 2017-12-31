466cfb84a5a32875c3aca7f2e44795c0febf9a8c
chenyong <chenyong6301567@126.com>
2017-12-31 19:33:38 +0800

    删除多余的。。。。。。。。。。

--- stars-biz/src/test/java/com/hotyum/stars/biz/test/BaseBizTest.java
+++ stars-biz/src/test/java/com/hotyum/stars/biz/test/BaseBizTest.java
@@ -1,8 +1,6 @@
 package com.hotyum.stars.biz.test;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.boot.test.context.SpringBootTest;
@@ -11,38 +9,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hotyum.stars.biz.config.cache.CacheConfiguration;
-//import com.hotyum.stars.biz.SpringBootBizMain;
-import com.hotyum.stars.biz.manager.ItemManager;
 import com.hotyum.stars.dal.config.DataSourceConfiguration;
 import com.hotyum.stars.dal.config.MyBatisConfigurer;
 import com.hotyum.stars.dal.config.MyBatisMapperScannerConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
-@ContextConfiguration(classes={DataSourceConfiguration.class,
-		MyBatisConfigurer.class,
-		MyBatisMapperScannerConfig.class,
-		CacheConfiguration.class})
-//这个注解的class必须在上层package中
-//@SpringBootTest(classes=SpringBootBizMain.class)
-//这个注解一定要放到最下面
-@ComponentScan(basePackages={"com.hotyum.stars.biz"})
+@ContextConfiguration(classes = { DataSourceConfiguration.class, MyBatisConfigurer.class,
+		MyBatisMapperScannerConfig.class, CacheConfiguration.class })
+// 这个注解的class必须在上层package中
+// @SpringBootTest(classes=SpringBootBizMain.class)
+// 这个注解一定要放到最下面
+@ComponentScan(basePackages = { "com.hotyum.stars.biz" })
 public class BaseBizTest {
-	
-	@Autowired
-	//@Qualifier("itemManager")
-	protected ItemManager itemManager;
-	
-	@Test
-	public void testItemCount() {
-		System.out.println(itemManager.getItemCount());
-	}
 
-	public ItemManager getItemManager() {
-		return itemManager;
-	}
-
-	public void setItemManager(ItemManager itemManager) {
-		this.itemManager = itemManager;
-	}
 }
--- stars-facade/src/main/java/com/hotyum/stars/facade/ItemServiceImpl.java
+++ stars-facade/src/main/java/com/hotyum/stars/facade/ItemServiceImpl.java
@@ -1,27 +1,27 @@
 package com.hotyum.stars.facade;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.alibaba.dubbo.config.annotation.Service;
-import com.hotyum.stars.biz.manager.ItemManager;
 import com.hotyum.stars.client.ItemService;
 import com.hotyum.stars.client.model.ItemVO;
-import com.hotyum.stars.dal.model.Item;
 
-@Service(version="1.0.0")
+@Service(version = "1.0.0")
 public class ItemServiceImpl implements ItemService {
 
-	@Autowired
-	private ItemManager itemManager;
-	
+	/**
+	* @Title:getItem
+	* @author:cy
+	* @Description 
+	* @date:2017年12月31日下午7:33:05
+	* @param 
+	* @param 
+	* @param 
+	* @return 
+	* @throws:
+	*/
 	@Override
 	public ItemVO getItem(long id) {
-		Item item  = itemManager.getItem(id);
-		ItemVO vo = new ItemVO();
-		vo.setBarcode(item.getBarcode());
-		vo.setId(item.getId());
-		vo.setName(item.getName());
-		return vo;
+		// TODO Auto-generated method stub
+		return null;
 	}
+
 }
