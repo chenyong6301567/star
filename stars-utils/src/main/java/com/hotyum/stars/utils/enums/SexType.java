d3706d82c272721b9d402106ac8332ed9da20767
chenyong <chenyong6301567@126.com>
2018-01-17 20:07:02 +0800

    ddddddddd

--- stars-utils/src/main/java/com/hotyum/stars/utils/enums/SexType.java
+++ stars-utils/src/main/java/com/hotyum/stars/utils/enums/SexType.java
@@ -8,7 +8,7 @@ package com.hotyum.stars.utils.enums;
 public enum SexType {
 	// 1男，2女
 
-	MALE("代理商", (byte) 1), FEMALE("客户", (byte) 2);
+	MALE("男", (byte) 1), FEMALE("女", (byte) 2);
 
 	private String description;
 	private byte value;
