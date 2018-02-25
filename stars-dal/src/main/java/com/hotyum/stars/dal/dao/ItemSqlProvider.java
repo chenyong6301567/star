e1e0f6233aa4a3dfcca36c7be580817389360913
chenyong <chenyong6301567@126.com>
2018-02-25 16:19:44 +0800

    数据库结构回复密码调整提交。。。。。

--- stars-dal/src/main/java/com/hotyum/stars/dal/dao/ItemSqlProvider.java
+++
@@ -1,9 +0,0 @@
-package com.hotyum.stars.dal.dao;
-
-public class ItemSqlProvider extends com.hotyum.stars.dal.mapper.ItemSqlProvider{
-
-	protected String selectCertifyByShopName(){
-		
-		return "";
-	}
-}
--- stars-dal/src/test/java/com/hotyum/stars/dal/test/BaseDalTest.java
+++ stars-dal/src/test/java/com/hotyum/stars/dal/test/BaseDalTest.java
@@ -2,7 +2,6 @@ package com.hotyum.stars.dal.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,22 +10,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.hotyum.stars.dal.config.DataSourceConfiguration;
 import com.hotyum.stars.dal.config.MyBatisConfigurer;
 import com.hotyum.stars.dal.config.MyBatisMapperScannerConfig;
-import com.hotyum.stars.dal.dao.ItemDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@EnableAutoConfiguration //(exclude={DataSourceAutoConfiguration.class})
-//@ComponentScan
-@ContextConfiguration(classes={DataSourceConfiguration.class,MyBatisConfigurer.class,MyBatisMapperScannerConfig.class})
+// @EnableAutoConfiguration //(exclude={DataSourceAutoConfiguration.class})
+// @ComponentScan
+@ContextConfiguration(classes = { DataSourceConfiguration.class, MyBatisConfigurer.class,
+		MyBatisMapperScannerConfig.class })
 public class BaseDalTest {
 
-	@Autowired
-	private ItemDAO itemDAO;
-	
 	@Test
 	public void testItemCount() {
-		long count = itemDAO.countByExample(null);
-		System.out.println(count);
-		
+
 	}
-	
+
 }
